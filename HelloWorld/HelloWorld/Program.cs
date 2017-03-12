72858415219ad92f3d79bd343cf4c12aad68be7f
altairezio0814 <altairezio0814@gmail.com>
2017-03-12 17:20:42 +0530

    Just Adding Some Changes in the NameSpace Main
    in Main() Creating an Program Object and Testing It.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,13 +1,18 @@
-﻿using System;
+//Here I have just used some examples to have a basic idea about attributes and methods accesibility in C#
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Namespace Main
 namespace Main
-{
+{  
+    //Inheriting Aritra class from namespace Aritra
     class Program : Aritra.Aritra
     {
+        //Have to use this as the superclass Aritra has a parameterised constructor.
+        //So while creating an object of a subclass, it is important to call the superclass constructor.
         public Program(string name, int age) : base(name, age)
         {
         }
@@ -25,6 +30,8 @@ namespace Main
             Aritra.Another another = new Aritra.Another();
             another.setAnother("Another", 22);
             another.display();
+            Program prog = new Program("Raktim", 22);
+            prog.displayDetails();
             Console.ReadKey();
         }
     }
